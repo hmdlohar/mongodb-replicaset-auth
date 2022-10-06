1e953690f1572f78cb6ba1f31bd8112f2091e7c5
hmdlohar <hmdlohar@gmail.com>
2022-10-06 14:27:38 +0530

    replication change

--- start-replication.js
+++ start-replication.js
@@ -1,48 +1,53 @@
-
 var replStatus = rs.status();
 
-if(replStatus && replStatus.ok === 1){
-	print(" ### Replication OK");
-	printjson(replStatus);
+if (replStatus && replStatus.ok === 1) {
+  print(" ### Replication OK");
+  printjson(replStatus);
 } else {
-	print(" ### Starting replication...");
-	printjson(rs.initiate(rs_cluster));
-	sleep(5000);
-	print(" ");
-	print(" ### Replica Set status");
-	printjson(rs.status());
+  print(" ### Starting replication...");
+  printjson(rs.initiate(rs_cluster));
+  sleep(5000);
+  print(" ");
+  print(" ### Replica Set status");
+  replStatus = rs.status()
+  printjson(replStatus);
 }
 
 sleep(5000);
 
-const dbUser = "dbAdmin"
-const dbPass = "dbAdminPass"
+const dbUser = "dbAdmin";
+const dbPass = "dbAdminPass";
 
+if (replStatus && replStatus.ok === 1) {
+  db = db.getSiblingDB("admin");
 
-if(replStatus && replStatus.ok === 1){
-	
-	db = db.getSiblingDB("admin");
+  if (!db.auth(dbUser, dbPass)) {
+    print(" ### Creating DB Admin user...");
 
-	if(!db.auth(dbUser,dbPass)){
-		print(" ### Creating DB Admin user...");
+    var adminUser = db.createUser({
+      user: dbUser,
+      pwd: dbPass,
+      roles: [
+        { role: "userAdminAnyDatabase", db: "admin" },
+        { role: "root", db: "admin" },
+      ],
+    });
 
-		var adminUser = db.createUser({ user: dbUser, pwd: dbPass, roles: [ { role: "userAdminAnyDatabase", db: "admin" }, {role:"root", db: "admin"} ] });
+    var dbAdimOk = db.auth(dbUser, dbPass);
 
-		var dbAdimOk = db.auth("admin","latam");
-
-		print("DB Admin authentication after create: "+dbAdimOk);
-	} else {
-		print(" ### Logged with DB Admin user");
-	}
+    print("DB Admin authentication after create: " + dbAdimOk);
+  } else {
+    print(" ### Logged with DB Admin user");
+  }
 } else {
-	print(" ### Replication is not running:");
-	printjson(replStatus);
+  print(" ### Replication is not running:");
+  printjson(replStatus);
 }
 
 var sleep = function (milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
+    if (new Date().getTime() - start > milliseconds) {
       break;
     }
   }
